e42ca822c051fdbf197accb28c77525432993310
Azatyk <azatuk2005@mail.ru>
2022-09-24 00:29:53 +0600

    feat: add mapbox example map

--- src/modules/map/views/MapView.vue
+++ src/modules/map/views/MapView.vue
@@ -1,3 +1,30 @@
 <template>
-  <div class="map-view">Map view</div>
+  <div id="map"></div>
 </template>
+
+<script lang="ts" setup>
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYXphdHlrIiwiYSI6ImNsOGVzZjhhMjBlN3czdnA5amgyYmhqM3UifQ.vJarQRq9FMd1I0OnEkVEAA";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-74.5, 40],
+  projection: "globe",
+});
+
+map.on("style.load", () => {
+  map.setFog({});
+});
+</script>
+
+<style>
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+</style>
