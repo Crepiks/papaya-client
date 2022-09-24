e120c25a6ed9bbf756da3f94d02f2fdfc8b9b210
Sayazhan Onlasyn <sayazhan.onlassyn@mail.ru>
2022-09-24 23:06:28 +0600

    fix: remove building view from router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MapView from "@/modules/map/views/MapView.vue";
-import BuildingView from "@/modules/building/views/BuildingView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +8,6 @@ const router = createRouter({
       path: "/",
       name: "map",
       component: MapView,
-    },
-    {
-      path: "/building",
-      name: "building",
-      component: BuildingView,
     },
   ],
 });
