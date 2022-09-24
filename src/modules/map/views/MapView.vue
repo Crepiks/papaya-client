a182614583dde37c24e1fd44bfe814d8a6d9e750
Azatyk <azatuk2005@mail.ru>
2022-09-24 12:40:44 +0600

    feat: add 3d mapbox map with native geocoder

--- src/modules/map/views/MapView.vue
+++ src/modules/map/views/MapView.vue
@@ -3,20 +3,89 @@
 </template>
 
 <script lang="ts" setup>
+import { onMounted } from "vue";
 import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiYXphdHlrIiwiYSI6ImNsOGVzZjhhMjBlN3czdnA5amgyYmhqM3UifQ.vJarQRq9FMd1I0OnEkVEAA";
-
-const map = new mapboxgl.Map({
+const mapOptions = {
   container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
-  center: [-74.5, 40],
-  projection: "globe",
-});
+  pitch: 45,
+  bearing: -17.6,
+  center: [71.399033, 51.090245],
+  zoom: 16,
+  antilias: true,
+  style: "mapbox://styles/mapbox/navigation-night-v1",
+};
 
-map.on("style.load", () => {
-  map.setFog({});
+const createMap = () => {
+  try {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiYXphdHlrIiwiYSI6ImNsOGVzZjhhMjBlN3czdnA5amgyYmhqM3UifQ.vJarQRq9FMd1I0OnEkVEAA";
+
+    const map = new mapboxgl.Map(mapOptions);
+
+    map.on("load", () => {
+      // Insert the layer beneath any symbol layer.
+      const layers = map.getStyle().layers;
+      const labelLayerId = layers.find(
+        (layer: any) => layer.type === "symbol" && layer.layout["text-field"]
+      ).id;
+
+      // The 'building' layer in the Mapbox Streets
+      // vector tileset contains building height data
+      // from OpenStreetMap.
+      map.addLayer(
+        {
+          id: "add-3d-buildings",
+          source: "composite",
+          "source-layer": "building",
+          filter: ["==", "extrude", "true"],
+          type: "fill-extrusion",
+          minzoom: 15,
+          paint: {
+            "fill-extrusion-color": "#aaa",
+
+            // Use an 'interpolate' expression to
+            // add a smooth transition effect to
+            // the buildings as the user zooms in.
+            "fill-extrusion-height": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "height"],
+            ],
+            "fill-extrusion-base": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "min_height"],
+            ],
+            "fill-extrusion-opacity": 0.6,
+          },
+        },
+        labelLayerId
+      );
+    });
+
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+      })
+    );
+  } catch (err) {
+    console.log("map error", err);
+  }
+};
+
+onMounted(() => {
+  createMap();
 });
 </script>
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,2 @@
 declare module "mapbox-gl";
+declare module "@mapbox/mapbox-gl-geocoder";
